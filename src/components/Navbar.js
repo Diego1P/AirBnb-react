fe07540adf9f1bb2a92b77629f0dd7795f8b6047
Diego1P <91356775+Diego1P@users.noreply.github.com>
2023-01-04 06:20:38 -0500

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <nav className="nav">
+            <img src='/images/airbnb-logo.png' alt='navbar' className="nav--logo" />
+        </nav>
+    )
+}
+
+export default Navbar;
